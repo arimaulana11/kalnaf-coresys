7d2cabb5db149f918d44e7573b45d28e2e74565c
Ari Maulana <m.ari.maulana93@gmail.com>
2026-01-26 14:20:42 +0700

    debug: register health controller and simplify vercel config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import jwtConfig from './config/jwt.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { StoresModule } from './modules/stores/stores.module';
-// import { HealthModule } from './modules/health/health.module';
+import { HealthController } from './health.controller';
 import { CategoryModule } from './modules/category/category.module';
 import { ProductModule } from './modules/product/product.module';
 import databaseConfig from './config/database.config';
@@ -25,7 +25,7 @@ import { InventoryModule } from './modules/inventory/inventory.module';
     CategoryModule,
     ProductModule,
     InventoryModule,
-    // HealthModule,
+    HealthController,
   ],
 })
 export class AppModule {}
